d5e15494ecd4ad206056d68439598f6d3f5a26a8
Klaas van Schelven <klaas@vanschelven.com>
2025-04-10 11:25:09 +0200

    debugID sourcemap that seemed to work but still doesn't
    ```
    sentry-cli sourcemaps inject .
    uglifyjs captureException.js --source-map  -o captureException.min.js
    sentry-cli sourcemaps inject .
    ```
    
            it has  2 debug Ids:
                120... orig
                c4a ... minified...
                    which _also_ contains the minified one!!
                c4a ... map
                120 ... in the event

--- captureException.js
+++ captureException.js
@@ -1,3 +1,5 @@
+
+!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:{},n=(new e.Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="120d9a0e-8234-5b98-9e7d-2b589d9c24ec")}catch(e){}}();
 function bar() {
     Sentry.captureException(new Error("Sentry Test Error"));
 }
@@ -9,3 +11,5 @@ function foo() {
 function captureException() {
     foo();
 }
+
+//# debugId=120d9a0e-8234-5b98-9e7d-2b589d9c24ec
