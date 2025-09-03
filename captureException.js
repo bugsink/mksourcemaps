d8300ef640e6a5acacda835c715fd2400ec65eb3
Klaas van Schelven <klaas@vanschelven.com>
2025-09-03 14:01:24 +0200

    UNminimized (original) example
    ```
    pip install ecma426
    identity-sourcemap captureException.js
    sentry-cli sourcemaps inject captureException.js captureException.js.map
    SENTRY_AUTH_TOKEN=... sentry-cli --url http://bugsink:8000/ sourcemaps --org singleorg --project=foobar upload captureException.js.map
    ```
    
    See https://github.com/bugsink/bugsink/issues/186

--- captureException.js
+++ captureException.js
@@ -1,3 +1,5 @@
+
+!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:{},n=(new e.Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="10025be0-a8d4-56ed-b282-13ecdc8eaffc")}catch(e){}}();
 function bar() {
     Sentry.captureException(new Error("Sentry Test Error"));
 }
@@ -9,3 +11,5 @@ function foo() {
 function captureException() {
     foo();
 }
+
+//# debugId=10025be0-a8d4-56ed-b282-13ecdc8eaffc
